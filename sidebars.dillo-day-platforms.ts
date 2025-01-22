d88715d40da17b5e54c84874599725e12d1ba8b2
Ethan Pineda <joshuapineda66@gmail.com>
2025-01-22 13:53:58 -0600

    adding starter files. TODO: actually add the content now

--- sidebars.dillo-day-platforms.ts
+++ sidebars.dillo-day-platforms.ts
@@ -1,11 +1,74 @@
 import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 
 const sidebars: SidebarsConfig = {
-  "dillo-day-platforms": [
+  docs: [
     {
       type: "doc",
       id: "intro",
       label: "Introduction",
+    },
+    {
+      type: "category",
+      label: "Dillo Day Website",
+      items: [
+        {
+          type: "doc",
+          id: "dilloday-website/overview",
+          label: "Overview",
+        },
+        {
+          type: "doc",
+          id: "dilloday-website/development-guide",
+          label: "Development Guide",
+        },
+        {
+          type: "doc",
+          id: "dilloday-website/deployment",
+          label: "Deployment",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Dillo Day Mobile App",
+      items: [
+        {
+          type: "doc",
+          id: "dillo-app/overview",
+          label: "Overview",
+        },
+        {
+          type: "doc",
+          id: "dillo-app/setup",
+          label: "Local Development Setup",
+        },
+        {
+          type: "doc",
+          id: "dillo-app/features",
+          label: "Feature Overview",
+        },
+        {
+          type: "doc",
+          id: "dillo-app/development-guide",
+          label: "Development Guide",
+        },
+        {
+          type: "doc",
+          id: "dillo-app/deployment",
+          label: "Deployment",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Dillo Day CLI Tool",
+      items: [
+        {
+          type: "doc",
+          id: "dilloday-cli/overview",
+          label: "Overview",
+        },
+      ],
     },
   ],
 };
